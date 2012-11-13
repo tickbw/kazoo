cf8f955960be44a5e408d1dc792f6ef4bc9dfbe9
James Aimonetti <james@2600hz.org>
2012-11-12 23:50:27 -0800

    WHISTLE-1186: add xmerl to main hrl, and move some defines here as well

--- whistle_apps/lib/kazoo_translator-1.0.0/src/kzt.hrl
+++ whistle_apps/lib/kazoo_translator-1.0.0/src/kzt.hrl
@@ -1,6 +1,7 @@
 -ifndef(KZT_HRL).
 
 -include_lib("whistle/include/wh_types.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
 
 -type ok_return() :: {'ok', whapps_call:call()}.
 -type stop_return() :: {'stop', whapps_call:call()}.
@@ -18,6 +19,15 @@
         {'request', whapps_call:call(), ne_binary(), 'get' | 'post', api_terms()}.
 
 -define(KZT_USER_VARS, <<"user_vars">>).
+
+-define(STATUS_QUEUED, <<"queued">>).
+-define(STATUS_RINGING, <<"ringing">>).
+-define(STATUS_ANSWERED, <<"in-progress">>).
+-define(STATUS_COMPLETED, <<"completed">>).
+-define(STATUS_BUSY, <<"busy">>).
+-define(STATUS_FAILED, <<"failed">>).
+-define(STATUS_NOANSWER, <<"no-answer">>).
+-define(STATUS_CANCELED, <<"canceled">>).
 
 -define(APP_NAME, <<"translator">>).
 -define(APP_VERSION, <<"0.4.0">>).
